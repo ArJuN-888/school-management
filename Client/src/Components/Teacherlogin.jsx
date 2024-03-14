67ca106e6e7158239034d0a34ef3df738a1d540d
unknown <jaikrishnanmo2002@gmail.com>
2024-03-14 18:52:24 +0530

    worked on student component

--- Client/src/Components/Teacherlogin.jsx
+++ Client/src/Components/Teacherlogin.jsx
@@ -1,53 +1,64 @@
-import React, { useContext } from 'react'
+import React, { useContext } from 'react';
 import axios from "axios";
 import mycontext from '../Context/Context';
-import { useNavigate,Link } from 'react-router-dom'
-export default function Login() {
-  const {teacherloginData,setteacherLogindata} = useContext(mycontext)
-  const nav= useNavigate()
-console.log("lgindt",teacherloginData)
-    const handleLoginChange = (key, value) => {
-        setteacherLogindata({ ...teacherloginData, [key]: value });
-    };
-    const login = async () => {
-        try {
-            const response = await axios.post("http://localhost:5000/Teacher/login",teacherloginData );
-            alert(response.data.message);
-            localStorage.setItem("teacherID",response.data.tID)
-            localStorage.setItem("teacherName",response.data.tname)
-            nav("/Teacherhome")
-           
-        } catch (error) {
-            alert(error.response.data.message);
-        }
-    };
-  return (
-    <div>
+import { useNavigate, Link } from 'react-router-dom';
+import "./Styles/Teacherlogin.css"
 
-<label>Teacher Login</label>
-<div>
-            <input
-                type='text'
-                value={teacherloginData.email}
-                placeholder='Email...'
-                onChange={(e) => handleLoginChange("email", e.target.value)}
-            />
-            
-            <input
-                type='text'
-                value={teacherloginData.batch}
-                placeholder='batch...'
-                onChange={(e) => handleLoginChange("batch", e.target.value)}
-            />
-            <input
-                type='text'
-                value={teacherloginData.password}
-                placeholder='Password...'
-                onChange={(e) => handleLoginChange("password", e.target.value)}
-            />
-            <button onClick={login}>Login</button>
-<Link to="/Tregister">Register</Link>
-            </div>
+export default function Login() {
+  const { teacherloginData, setteacherLogindata } = useContext(mycontext);
+  const nav = useNavigate();
+  console.log("lgindt", teacherloginData);
+
+  const handleLoginChange = (key, value) => {
+    setteacherLogindata({ ...teacherloginData, [key]: value });
+  };
+
+  const login = async () => {
+    try {
+      const response = await axios.post("http://localhost:5000/Teacher/login", teacherloginData);
+      alert(response.data.message);
+      localStorage.setItem("teacherID", response.data.tID);
+      localStorage.setItem("teacherName", response.data.tname);
+      nav("/Teacherhome");
+    } catch (error) {
+      alert(error.response.data.message);
+    }
+  };
+
+  return (
+    <div className="container">
+      <h2>Teacher Login</h2>
+      <div className="form-group">
+        <label>Email</label>
+        <input
+          type='text'
+          value={teacherloginData.email}
+          placeholder='Email...'
+          onChange={(e) => handleLoginChange("email", e.target.value)}
+        />
+      </div>
+      <div className="form-group">
+        <label>Batch</label>
+        <input
+          type='text'
+          value={teacherloginData.batch}
+          placeholder='Batch...'
+          onChange={(e) => handleLoginChange("batch", e.target.value)}
+        />
+      </div>
+      <div className="form-group">
+        <label>Password</label>
+        <input
+          type='password'
+          value={teacherloginData.password}
+          placeholder='Password...'
+          onChange={(e) => handleLoginChange("password", e.target.value)}
+        />
+      </div>
+      <button onClick={login}>Login</button>
+      <div className="register-link">
+        <Link to="/Tregister">Register</Link>
+      </div>
     </div>
-  )
+  );
 }
