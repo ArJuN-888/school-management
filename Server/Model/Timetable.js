b92255f8e50d47decfb3628c85a50798b2f8afa8
Harikrishnan KV <harikv03@gmail.com>
2024-03-17 21:09:53 +0530

    timetable schema

---
+++ Server/Model/Timetable.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const StudentTimetableSchema = new Schema({
+    classteacher: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'teachers',
+        required: true
+    },
+    timetable: {
+        Monday: [{
+            type: String
+        }],
+        Tuesday: [{
+            type: String
+        }],
+        Wednesday: [{
+            type: String
+        }],
+        Thursday: [{
+            type: String
+        }],
+        Friday: [{
+            type: String
+        }],
+        Saturday: [{
+            type: String
+        }] 
+    }
+});
+
+const StudentTimetable = mongoose.model('StudentTimetable', StudentTimetableSchema);
+
+module.exports = StudentTimetable;
