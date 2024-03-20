3cb1d32652441961ac3362e75029b15f8cafd8d3
unknown <jaikrishnanmo2002@gmail.com>
2024-03-20 11:59:59 +0530

    worked on attendence View

--- Client/src/Components/Parentattendenceviewing.jsx
+++ Client/src/Components/Parentattendenceviewing.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
-
+import React, { useContext, useEffect, useState } from 'react'
+import GetParentID from "./Hooks/GetParentID"
+import GetParentName from "./Hooks/GetParentName"
+import axios from 'axios'
+import mycontext from '../Context/Context'
 const Parentattendenceviewing = () => {
+
+    const {baseURL}=useContext(mycontext)
+
+    const [attendence,seAttendence]=useState([])
+    console.log("attendence",attendence)
+
+    const parentName=GetParentName()
+    const parentid=GetParentID()
+
+    useEffect(()=>{
+        AttendenceView()
+    },[parentid])
+
+
+    const AttendenceView=async()=>{
+        try {
+            const response= await axios.get(`${baseURL}/attendence/record/${parentid}`)
+            console.log(response.data.message)
+            seAttendence(response.data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
   return (
     <div>
-      <div className='heading'> helo...</div>
+      <div className='heading'>helo... {parentName} </div>
+
     </div>
   )
 }
