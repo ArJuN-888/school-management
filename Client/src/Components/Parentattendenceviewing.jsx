b774e4d996c0987159c851a856207d80ce9cf581
unknown <jaikrishnanmo2002@gmail.com>
2024-03-20 12:28:22 +0530

    worked on attendence

--- Client/src/Components/Parentattendenceviewing.jsx
+++ Client/src/Components/Parentattendenceviewing.jsx
@@ -3,6 +3,7 @@ import GetParentID from "./Hooks/GetParentID"
 import GetParentName from "./Hooks/GetParentName"
 import axios from 'axios'
 import mycontext from '../Context/Context'
+import { Table } from 'react-bootstrap'
 const Parentattendenceviewing = () => {
 
     const {baseURL}=useContext(mycontext)
@@ -22,7 +23,7 @@ const Parentattendenceviewing = () => {
         try {
             const response= await axios.get(`${baseURL}/attendence/record/${parentid}`)
             console.log(response.data.message)
-            seAttendence(response.data)
+            seAttendence(response.data.message)
         } catch (error) {
             console.log(error)
         }
@@ -30,7 +31,28 @@ const Parentattendenceviewing = () => {
   return (
     <div>
       <div className='heading'>helo... {parentName} </div>
-
+      <div className='attendence'>
+        {attendence.length===0 ?(
+            <div><h3>no record found</h3></div>
+        ):(
+            <Table>
+            <thead>
+                <th>student-id</th>
+                <th>Date</th>
+                <th>Status</th>
+            </thead>
+            <tbody>
+               {attendence.map((data,index)=>(
+                <tr key={index}>
+                    <td>{data.studentid}</td>
+                    <td>{data.date}</td>
+                    <td>{data.status}</td>
+                </tr>
+               ))}
+            </tbody>
+         </Table>
+        )}
+      </div>
     </div>
   )
 }
