b7f47e0f3df8bf5ea89c6ba3d8cae97c5ab865d6
unknown <jaikrishnanmo2002@gmail.com>
2024-03-22 07:43:29 +0530

    worked

--- Client/src/Components/DoctorHealthView.jsx
+++ Client/src/Components/DoctorHealthView.jsx
@@ -6,7 +6,6 @@ import { Table } from 'react-bootstrap';
 const DoctorHealthView = () => {
     const { baseURL } = useContext(mycontext);
     const [record, setRecord] = useState([]);
-    console.log("mystudents",record)
 
     useEffect(() => {
         ViewRecord();
@@ -25,17 +24,17 @@ const DoctorHealthView = () => {
     return (
         <div>
             <div className='heading'>
-                <h3>Health Records of students</h3>
+                <h3>Health Records of Students</h3>
             </div>
 
             <div className='table'>
-                <Table>
+                <Table striped bordered hover variant="blue">
                     <thead>
                         <tr>
-                            <th>Student-id</th>
+                            <th>Student ID</th>
                             <th>Student Name</th>
                             <th>Batch</th>
-                            <th>Health report</th>
+                            <th>Health Report</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -45,14 +44,22 @@ const DoctorHealthView = () => {
                                     <td>{data._id}</td>
                                     <td>{data.studentname}</td>
                                     <td>{data.batch}</td>
-                                    <td> vision:{data.Vision}, Mentelhealth:{data.MentalHealth},Immunization:{data.Immunization},Hearing:{data.Hearing},NutritionStatus:{data.NutritionStatus},PhysicalExamination:{data.PhysicalExamination}</td>
+                                    <td> 
+                                        <ul>
+                                            <li><strong>Vision:</strong> {data.Vision}</li>
+                                            <li><strong>Mental Health:</strong> {data.MentalHealth}</li>
+                                            <li><strong>Immunization:</strong> {data.Immunization}</li>
+                                            <li><strong>Hearing:</strong> {data.Hearing}</li>
+                                            <li><strong>Nutrition Status:</strong> {data.NutritionStatus}</li>
+                                            <li><strong>Physical Examination:</strong> {data.PhysicalExamination}</li>
+                                        </ul>
+                                    </td>
                                 </tr>
-                                
                             ))
                         ) : (
                             <tr>
                                 <td colSpan="4">
-                                    <h3>No students available</h3>
+                                    <h3>No Students Available</h3>
                                 </td>
                             </tr>
                         )}
